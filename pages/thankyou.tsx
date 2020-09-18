4bbd21615f2bce5d657b38680cbfcbe98c4ad8b8
Benjamin Aplin <benjaminaplin@gmail.com>
2020-09-17 23:42:13 -0500

    add form

---
+++ pages/thankyou.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const ThankYou = () => {
+  return (
+    <div>
+      thanks bruh
+      <Link href='/'>
+        go back
+      </Link>
+    </div>
+  )
+}
+
+export default ThankYou;
